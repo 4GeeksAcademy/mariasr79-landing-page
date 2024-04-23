7af5876bcf3be5f27a93a088f5fbd172e97d79cd
mariasr79 <147938132+mariasr79@users.noreply.github.com>
2024-04-23 15:18:31 +0000

    finalizado

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Footer = () => {
+    return (
+
+        <div class="card">
+            <div class="card-header bg-secondary">
+            Copywrite @ Your Webside 2019
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
